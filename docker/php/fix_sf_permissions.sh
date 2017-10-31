ed41db6a1d363861bba0609342876949dd36f5a3
Iuxmat <m.galacci@gmail.com>
2017-10-31 01:24:05 +0100

    sistemazione docker

--- docker/php/fix_sf_permissions.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-HTTPDUSER=$(ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1)
-setfacl -dR -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX /var/www/app/var
-setfacl -R -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX /var/www/app/var
